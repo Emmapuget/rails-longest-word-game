7880cb72dc81ab391d163d3f30cd09cd65dab27e
emma puget <puget.emma@gmail.com>
2024-02-19 18:04:59 +0100

    final

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "score", to: "games#score"
+  get "new", to: "games#new"
+  post "new", to: "games#new"
+
+  root "games#new"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
